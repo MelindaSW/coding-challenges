09caf000f6abad7d85dfa1462230f1bcaff39f58
Melinda <sandstrommelinda@gmail.com>
2020-12-01 16:16:19 +0100

    Minor clean ups

--- advent-of-code/2020/src/DayOnePartTwo.java
+++ advent-of-code/2020/src/DayOnePartTwo.java
@@ -46,29 +46,3 @@ public class DayOnePartTwo {
         return 0;
     }
 }
-//
-// /* Now fix the first element one by one and find the
-//           other two elements */
-//        for (int i = 0; i < arr_size - 2; i++) {
-//
-//        // To find the other two elements, start two index variables
-//        // from two corners of the array and move them toward each
-//        // other
-//        l = i + 1; // index of the first element in the remaining elements
-//        r = arr_size - 1; // index of the last element
-//        while (l < r) {
-//        if (A[i] + A[l] + A[r] == sum) {
-//        System.out.print("Triplet is " + A[i] + ", " + A[l] + ", " + A[r]);
-//        return true;
-//        }
-//        else if (A[i] + A[l] + A[r] < sum)
-//        l++;
-//
-//        else // A[i] + A[l] + A[r] > sum
-//        r--;
-//        }
-//        }
-//
-//        // If we reach here, then no triplet was found
-//        return false;
-//        }
